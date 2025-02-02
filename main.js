e8e5d59bdcd485a4b77ade427de81514e12cd031
Atif Pathan <atifkhan.p1@gmail.com>
2025-02-02 13:04:04 -0700

    add html css skeleton for future gui, init knightMoves function

---
+++ main.js
@@ -0,0 +1,9 @@
+function knightMoves (start, end) {
+    // create 8x8 array to keep track of visited nodes
+    const visited = Array(8)
+    .fill(0) // Initialize with `0` initially
+    .map(() => Array(8).fill(false)); // fill all with false
+        
+}
+
+knightMoves([0,0], [2,1]);
